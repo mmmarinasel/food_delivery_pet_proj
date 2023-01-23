6ace7174fbed2797c11fe17bc52351d922cb3a24
mmmarinasel <mmmarinasel@icloud.com>
2023-01-23 20:08:12 +0600

    add SwiftLint

--- mmmarinasel_food_delivery/ViewController.swift
+++ mmmarinasel_food_delivery/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  mmmarinasel_food_delivery
-//
-//  Created by Марина Селезнева on 23.01.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
